3650d22e32cb9ff4a48e3e8abcc9723e081aa9b6
Koray Löker <loker@radiobrecht.org>
2018-11-13 12:23:42 +0300

    Update 404.php

---
+++ 404.php
@@ -0,0 +1,20 @@
+<?php require '_head.inc'; ?>
+      </div>
+        <div class="row h-100">
+          <div class="col"></div>
+          <div class="col text-center">
+            <h1>Sayfa Bulunamadı</h1>
+            <div>Adreste bir hata var, lütfen sağ üstteki arama kutusunu deneyin.</div>
+          </div>
+          <div class="col"></div>
+        </div>
+        <div class="row h-100 py-3 my-3">
+          <div class="col"></div>
+          <div class="col text-center">
+            <h1>Page Not Found</h1>
+            <div>Your link has errors, please try to find the content in search box on top-right.</div>
+          </div>
+          <div class="col"></div>
+        </div>
+<?php
+require '_foot.inc';
